2137207f21fe2015e878391824ecd24214b123db
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-06-07 01:03:47 +0200

    Fixed responsiveness issues and removed unused imports

--- client/app/components/FAQ.tsx
+++ client/app/components/FAQ.tsx
@@ -2,10 +2,10 @@ function FAQ() {
   return (
     <div className=" flex justify-center ">
       <ul className="w-3/4 list-none text-left text-black dark:text-gray-200">
-        <h2 className="pb-3 text-3xl font-bold text-gray-100">
+        <h2 className="pb-3 text-2xl font-bold text-gray-100 sm:text-3xl">
           How is the total CO2 over Berlin calculated?
         </h2>
-        <li className="pb-3 text-xl">
+        <li className="text-l pb-3 sm:text-2xl">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit.
           Voluptatibus, nisi! Dolores libero expedita laudantium quae in error
           at debitis officiis harum. Quos dignissimos, hic minus ratione ipsa
@@ -19,10 +19,10 @@ function FAQ() {
           illum sint quibusdam maxime iusto voluptates assumenda obcaecati
           expedita incidunt nostrum totam!
         </li>
-        <h2 className="pb-3 text-3xl font-bold text-gray-100">
+        <h2 className="pb-3 text-2xl font-bold text-gray-100 sm:text-3xl">
           How is the total CO2 since you visited our site calculated?
         </h2>
-        <li className="pb-3 text-xl">
+        <li className="text-l pb-3 sm:text-xl">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur
           iure sapiente, tempora recusandae architecto nihil non a dignissimos
           exercitationem error delectus! Ducimus, consequuntur! Unde laborum
@@ -36,10 +36,10 @@ function FAQ() {
           sapiente cumque quasi ullam cupiditate impedit, odit eligendi nam eos
           vel quas vitae ab voluptas minus?
         </li>
-        <h2 className="pb-3 text-3xl font-bold text-gray-100">
+        <h2 className="pb-3 text-2xl font-bold text-gray-100 sm:text-3xl">
           Where do we get our data from?
         </h2>
-        <li className="text-xl">
+        <li className="text-l sm:text-xl">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus a
           nesciunt tempora aperiam officiis dignissimos excepturi animi aut in
           vero! Esse veniam commodi suscipit voluptatum ratione id adipisci
