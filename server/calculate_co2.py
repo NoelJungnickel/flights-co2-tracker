119a39a8137f535f9341e6c3b2dbd79123d17d9e
Satya <swinargaa@gmail.com>
2023-06-12 06:19:00 +0200

    added docstrings and cleanup

--- server/calculate_co2.py
+++ server/calculate_co2.py
@@ -1,16 +1,14 @@
 from opensky_network import get_states
-from geopy import distance as geopy_distance
+from geopy import distance as geopy_distance  # type: ignore
 import time
 import math
 from redis import Redis
 from argparse import ArgumentParser
 
-API_HOST = "127.0.0.1"
-API_PORT = 8000
 REDIS_HOST = "127.0.0.1"
 REDIS_PORT = 6379
-aircrafts_in_airspace = {}
-total_co2_emission = 0
+aircrafts_in_airspace: dict[str, dict] = {}
+total_co2_emission = 0.0
 redis = Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 
 
@@ -23,7 +21,8 @@ redis = Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 berlin_bounding_box = (52.3418234221, 13.0882097323, 52.6697240587, 13.7606105539)
 
 
-def main():
+def main() -> None:
+    """Entry point of the application."""
     # Connecting to Redis Database
     parser = ArgumentParser()
     parser.add_argument("--username", type=str, help="Username for OpenSky")
@@ -39,9 +38,10 @@ def main():
     except Exception:
         raise RuntimeError("Failed to connect to Redis.")
 
-    if redis.hget("total", "berlin"):
+    byte_total_co2_emission = redis.hget("total", "berlin")
+    if byte_total_co2_emission is not None:
         global total_co2_emission
-        total_co2_emission = float(redis.hget("total", "berlin").decode())
+        total_co2_emission = float(byte_total_co2_emission.decode())
 
     # call opensky api every (1 + calculation time) minute(s)
     while True:
@@ -49,14 +49,113 @@ def main():
 
         # response["states"] can be null
         if response is not None and response["states"] is not None:
-            update_aircrafts_in_airspace(response["states"], response["time"])
+            update_total_co2_emission(response["states"], response["time"])
 
         time.sleep(60)
 
 
-def update_aircrafts_in_airspace(states, request_time):
+def update_total_co2_emission(
+    states: list, request_time: int, exit_time_threshold: int = 60
+) -> None:
+    """Updates the global variable total_co2_emission and store it in Redis.
+
+        1. Keeps track of the aircraft state from current and previous requests
+            and store it in the aircrafts_in_airspace global variable.
+        2. Determines which aircrafts are no longer in the airspace.
+        3. Calculates the duration of those aircrafts in the airspace.
+        4. Calculates the CO2 emission of those aircrafts based on their
+            duration in the airspace.
+        5. Sums the CO2 emission of each aircraft.
+        6. Add the sum of the CO2 emission of each aircraft to the total_co2_emission
+            global variable.
+        7. Store the value of total_co2_emission in Redis.
+        8. Remove aircrafts that are no longer in the airspace.
+
+    Args:
+        states (list): A list of aircraft states received from the /states/all endpoint
+            of the OpenSky Network API.
+        request_time (int): The time that the request was sent in seconds since epoch.
+        exit_time_threshold (int): The amount of time needed to determine that
+            the  aircraft is no longer in the airspace. Defaults to 60 seconds.
+    """
     global total_co2_emission
     print(total_co2_emission)
+    update_aircrafts_in_airspace(states, request_time)
+
+    # find out which aircrafts are no longer in the airspace
+    aircraft_id_not_in_airspace = []
+    for aircraft_id in aircrafts_in_airspace:
+        aircraft = aircrafts_in_airspace[aircraft_id]
+
+        # if the last position update was more than x seconds ago,
+        # then assume its not in the airspace anymore
+        if request_time - aircraft["last_update"] >= exit_time_threshold:
+            aircraft_id_not_in_airspace.append(aircraft_id)
+
+    # calculate the CO2 emission of aircrafts that are no longer in the airspace
+    # as the state object of an aircraft may still appear in the next request cycle.
+    co2_emission_per_aircraft = []
+    for aircraft_id in aircraft_id_not_in_airspace:
+        # calculate the duration that the aircraft was in the airspace
+        positions = aircrafts_in_airspace[aircraft_id]["positions"]
+        durations = []
+
+        for index, position in enumerate(positions):
+            if index == len(positions) - 1:
+                if position["on_ground"]:
+                    continue
+                # calculate duration to the edge of the bounding box
+                # the edge is dictated by the plane's true_track
+
+                # Get the position, where the aircraft would intersect
+                # with the edge of the bounding box, if it were to keep
+                # heading in the same direction until it reaches the edge
+                # of the bounding box
+                edge_position = get_edge_position(
+                    position["true_track"],
+                    berlin_bounding_box,
+                    (position["longitude"], position["latitude"]),
+                )
+
+                # get the duration it would take to get to the edge position
+                duration = calculate_duration(
+                    (position["latitude"], position["longitude"]),
+                    (edge_position[0], edge_position[1]),
+                    position["velocity"],
+                )
+                durations.append(duration)
+                continue
+
+            next_position = positions[index + 1]
+
+            duration = calculate_duration(
+                (position["latitude"], position["longitude"]),
+                (next_position["latitude"], next_position["longitude"]),
+                position["velocity"],
+            )
+            durations.append(duration)
+        total_duration = sum(durations)
+
+        # calculate the CO2 emission
+        co2_emission = calculate_co2_emission(total_duration)
+        co2_emission_per_aircraft.append(co2_emission)
+
+    total_co2_emission += sum(co2_emission_per_aircraft)
+    redis.hset("total", "berlin", total_co2_emission)
+
+    # remove aircrafts no longer in airspace
+    for aircraft_id in aircraft_id_not_in_airspace:
+        del aircrafts_in_airspace[aircraft_id]
+
+
+def update_aircrafts_in_airspace(states: list, request_time: int) -> None:
+    """Updates the global variable aircrafts_in_airspace.
+
+    Args:
+        states (list): A list of aircraft states received from the /states/all endpoint
+            of the OpenSky Network API.
+        request_time (int): The time that the request was sent in seconds since epoch
+    """
     for state in states:
         time_position = state[3]
         longitude = state[5]
@@ -91,95 +190,53 @@ def update_aircrafts_in_airspace(states, request_time):
                 "positions": [position],
             }
 
-    # find out which aircrafts are no longer in the airspace
-    aircraft_id_not_in_airspace = []
-    for aircraft_id in aircrafts_in_airspace:
-        aircraft = aircrafts_in_airspace[aircraft_id]
 
-        # last update was x seconds ago, then assume its not in the
-        # airspace anymore
-        if request_time - aircraft["last_update"] >= 60:
-            aircraft_id_not_in_airspace.append(aircraft_id)
-
-    co2_emission_per_aircraft = []
-    for aircraft_id in aircraft_id_not_in_airspace:
-        # calculate the duration that it was in the airspace
-        positions = aircrafts_in_airspace[aircraft_id]["positions"]
-        durations = []
-
-        for index, position in enumerate(positions):
-            if index == len(positions) - 1:
-                if position["on_ground"]:
-                    continue
-                # calculate duration to the edge of the bounding box
-                # the edge is dictated by the plane's true_track
-                # north: 0,
-                # east: 90,
-                # south: 180,
-                # west: 270
-
-                # first get the position, where the aircraft would intersect with edge of the bounding box
-                # if it were to keep heading at the same direction until it reaches the edge of the bounding box
-                edge_position = get_edge_position(
-                    position["true_track"],
-                    berlin_bounding_box,
-                    (position["longitude"], position["latitude"]),
-                )
-
-                # get the duration it would take to get to the edge position
-                duration = calculate_duration(
-                    (position["latitude"], position["longitude"]),
-                    (edge_position[0], edge_position[1]),
-                    position["velocity"],
-                )
-                durations.append(duration)
-                continue
-
-            next_position = positions[index + 1]
-
-            # assume velocity is constant from point a to point b
-            duration = calculate_duration(
-                (position["latitude"], position["longitude"]),
-                (next_position["latitude"], next_position["longitude"]),
-                position["velocity"],
-            )
-            durations.append(duration)
-        total_duration = sum(durations)
-
-        # calculate the co2 emission
-        co2_emission = calculate_co2_emission(total_duration)
-        co2_emission_per_aircraft.append(co2_emission)
-
-    total_co2_emission += sum(co2_emission_per_aircraft)
-    redis.hset("total", "berlin", total_co2_emission)
-
-    # remove aircrafts no longer in airspace
-    for aircraft_id in aircraft_id_not_in_airspace:
-        del aircrafts_in_airspace[aircraft_id]
-
-
-# point tuple should be (latitude, longitude)
-# velocity in m/s
 def calculate_duration(
     point1: tuple[float, float], point2: tuple[float, float], velocity: float
-):
-    if velocity == 0:
-        return 0
+) -> float:
+    """Calculates the duration of travel from one point to another with constant velocity.
 
-    # get distance between the two points in km
+    Args:
+        point1 (tuple[float,float]): The first geographical point in degrees and
+            in the format (latitude, longitude).
+        point2 (tuple[float, float]): The second geographical point in degrees and
+            in the format (latitude, longitude).
+        velocity (float): The speed of the aircraft in meters per second.
+
+    Returns:
+        float: The duration in seconds to travel from point1 to point2 with
+            constant velocity.
+    """
+    if velocity == 0:
+        return 0.0
+
+    # Get the distance between the two points in kilometers
     distance = geopy_distance.great_circle(point1, point2).km
+    # Calculate the time required to travel the distance at the given velocity.
     time = (distance * 1000) / velocity
     return time
 
 
-# true_track in decimal degrees clockwise from north (north = 0)
-# bounding_box is (lamin, lomin, lamax, lomax)
-# position is (longitude, latitude)
 def get_edge_position(
     true_track: float,
     bounding_box: tuple[float, float, float, float],
     position: tuple[float, float],
-):
+) -> tuple[float, float]:
+    """Calculates the distance to the bounding box edge.
+
+    Args:
+        true_track (float): The direction of the aircraft in decimal degrees,
+            measured clockwise from north (north = 0).
+        bounding_box (tuple[float, float, float, float]): A tuple containing
+            the coordinates of the bounding box in the format
+            [lamin, lomin, lamax, lomax].
+        position (tuple[float, float]): The geographical point of the aircraft
+            in degrees and in the format (longitude, latitude).
+
+    Returns:
+        tuple[float, float]: The geographical coordinates (latitude, longitude)
+            representing the edge position towards which the aircraft is heading.
+    """
     lamin = bounding_box[0]
     lomin = bounding_box[1]
     lamax = bounding_box[2]
@@ -187,6 +244,8 @@ def get_edge_position(
 
     pos_la = position[1]
     pos_lo = position[0]
+
+    true_track %= 360
 
     # Quadrant I - true_track 0 deg - 90 deg
     # can be top or right edge
@@ -264,13 +323,26 @@ def get_edge_position(
 
         return (edge_pos_la, edge_pos_lo)
 
+    assert False, "Unreachable code - No quadrant matches"
 
-# duration in seconds
-# fuel_consumption_rate in kg per hour
-def calculate_co2_emission(duration, fuel_consumption_rate=378.54):
+
+def calculate_co2_emission(
+    duration: float, fuel_consumption_rate: float = 378.54
+) -> float:
+    """Calculates the amount of CO2 emission of a flight.
+
+    Args:
+        duration (float): The duration of the flight in seconds.
+        fuel_consumption_rate (float, optional): The rate of fuel consumption
+            of an aircraft in kilograms per hour. Defaults to 378.54 kg/hour.
+
+    Returns:
+        float: The amount of CO2 emission in kilograms.
+    """
     fuel_used_kg = fuel_consumption_rate * (duration / 3600)
     co2_kg = fuel_used_kg * 3.15
     return co2_kg
 
 
-main()
+if __name__ == "__main__":
+    main()
