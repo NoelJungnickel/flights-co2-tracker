6d1287f32ccb52b0c0ab29fae6d2fe3fdc638ed3
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-06-14 13:52:17 +0200

    Integrated API into frontend (since last visit funcitonality not working yet)

--- client/app/components/AirspaceCard.tsx
+++ client/app/components/AirspaceCard.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import AirspaceDropdownButton from "./AirspaceDropdownButton";
 
 const airspaceOptions = ["Berlin"] as const;
@@ -8,7 +9,36 @@ type Props = {
   totalCO2LocationKG: number;
 };
 
+export const CO2_KG_LAST_VISIT_STORAGE_KEY = "co2_last_visit";
+
 function AirspaceCard({ totalCO2LocationKG }: Props) {
+  const [lastVisitLocationCO2KG, setLastVisitLocationCO2KG] = useState(0);
+
+  const handleTabClosing = () => {
+    window.localStorage.setItem(
+      CO2_KG_LAST_VISIT_STORAGE_KEY,
+      totalCO2LocationKG.toString()
+    );
+  };
+
+  useEffect(() => {
+    let localCo2LastVisitKG = 0;
+    const maybeCO2lastVisitKgString = window.localStorage.getItem(
+      CO2_KG_LAST_VISIT_STORAGE_KEY
+    );
+
+    if (maybeCO2lastVisitKgString !== null) {
+      localCo2LastVisitKG = parseFloat(maybeCO2lastVisitKgString);
+    }
+
+    setLastVisitLocationCO2KG(localCo2LastVisitKG);
+
+    window.addEventListener("unload", handleTabClosing);
+    return () => {
+      window.removeEventListener("unload", handleTabClosing);
+    };
+  }, []);
+
   return (
     <div className="h-fit w-full rounded-lg bg-zinc-700 py-5">
       <div className="flex w-full justify-center gap-3 pb-6">
@@ -23,25 +53,31 @@ function AirspaceCard({ totalCO2LocationKG }: Props) {
       </div>
       <div className="flex flex-col items-center divide-y divide-zinc-800/30 text-6xl font-bold text-sky-50 sm:flex-row sm:divide-x sm:divide-y-0 md:text-8xl">
         <div className="w-1/2 py-5 text-center">
-          {totalCO2LocationKG}
-          <span className="text-xl md:text-5xl">kg</span>
+          {Math.floor(totalCO2LocationKG / 1000)}
+          <span className="text-xl md:text-5xl">t</span>
           <p className="block w-full pt-2 text-base font-normal text-sky-50 sm:hidden">
-            Since 6 June 2023
+            since 10 June 2023
           </p>
         </div>
         <div className="w-1/2 py-5 text-center">
-          10<span className="text-xl md:text-5xl">kg</span>
+          {(() => {
+            const now = Math.floor(totalCO2LocationKG / 1000);
+            const lastTime = Math.floor(lastVisitLocationCO2KG / 1000);
+            const difference = now - lastTime;
+            return difference <= now ? 0 : difference;
+          })()}
+          <span className="text-xl md:text-5xl">t</span>
           <p className="block w-full pt-2 text-base font-normal text-sky-50 sm:hidden">
-            Since you opened our site
+            more since your last visit
           </p>
         </div>
       </div>
       <div className="hidden w-full pt-4 text-center sm:flex">
         <p className="w-1/2 text-lg text-sky-50 md:text-xl">
-          Since 6 June 2023
+          since 10 June 2023
         </p>
         <p className="w-1/2 text-lg text-sky-50 md:text-xl">
-          Since you opened our site
+          more since your last visit
         </p>
       </div>
     </div>
--- client/app/routes/_app.stats.tsx
+++ client/app/routes/_app.stats.tsx
@@ -1,3 +1,4 @@
+import { TypedResponse, json } from "@remix-run/node";
 import {
   isRouteErrorResponse,
   useLoaderData,
@@ -6,16 +7,20 @@ import {
 import AirspaceCard from "~/components/AirspaceCard";
 import type { LeaderboardEntry } from "~/components/LeaderboardCard";
 import LeaderboardCard from "~/components/LeaderboardCard";
-import LeaderboardRow from "~/components/LeaderboardRow";
 
 type Stats = {
-  totalCO2LocationKG: number;
+  totalLocationCO2KG: number;
   leaderboardContent: LeaderboardEntry[];
 };
 
-export async function loader(): Promise<Stats> {
-  return {
-    totalCO2LocationKG: 2000,
+export async function loader(): Promise<TypedResponse<Stats>> {
+  const totalBerlinCO2KgReponse = await fetch(
+    "http://35.210.64.77:8000/api/total/berlin"
+  );
+  const totalBerlinCO2Kg = await totalBerlinCO2KgReponse.json();
+
+  return json({
+    totalLocationCO2KG: totalBerlinCO2Kg,
     leaderboardContent: [
       { placing: 1, name: "Taylor Swift", kgCO2: 200 },
       { placing: 2, name: "Elon Musk", kgCO2: 190 },
@@ -23,16 +28,16 @@ export async function loader(): Promise<Stats> {
       { placing: 4, name: "kenn ich nicht", kgCO2: 80 },
       { placing: 5, name: "nicht echt", kgCO2: 1 },
     ],
-  };
+  });
 }
 
 function Card() {
-  const { totalCO2LocationKG, leaderboardContent } =
+  const { totalLocationCO2KG, leaderboardContent } =
     useLoaderData<typeof loader>();
 
   return (
     <div className="flex w-full flex-col gap-3 lg:w-3/4 xl:w-2/3">
-      <AirspaceCard totalCO2LocationKG={totalCO2LocationKG} />
+      <AirspaceCard totalCO2LocationKG={totalLocationCO2KG} />
       <div className="h-px w-11/12 self-center bg-zinc-700/20"></div>
       <LeaderboardCard leaderboardContent={leaderboardContent} />
     </div>
