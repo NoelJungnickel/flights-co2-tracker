721242d260bc8aa61fbecca74e455263fd220de0
kilianp14 <Paul-Kilian@gmx.de>
2023-06-20 15:42:20 +0200

    Added some more cities with a way to work with different opensky accounts

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 import schedule
 import threading
 import time
+import configparser
 from redis import Redis
 from typing import Callable, Tuple
 from argparse import ArgumentParser
@@ -16,13 +17,24 @@ redis = Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
 def main() -> None:
     """Entry point of the application."""
     parser = ArgumentParser()
-    parser.add_argument("--username", type=str, help="Username for OpenSky")
-    parser.add_argument("--password", type=str, help="Password for OpenSky")
+    parser.add_argument("--config", type=str, help="Path to the configuration file")
     args = parser.parse_args()
 
-    # Access username and password from console
-    username = args.username
-    password = args.password
+    config_path = args.config
+
+    # Read credentials from config file
+    usernames = {}
+    passwords = {}
+
+    config = configparser.ConfigParser()
+    config.read(config_path)
+
+    for section in config.sections():
+        if "username" in config[section] and "password" in config[section]:
+            username = config[section]["username"]
+            password = config[section]["password"]
+            usernames[section] = username
+            passwords[section] = password
 
     # Connect to Redis Database
     try:
@@ -30,17 +42,29 @@ def main() -> None:
     except Exception:
         raise RuntimeError("Failed to connect to Redis.")
 
+    # Specify bounding boxes for airspaces to be watched
     bounding_boxes = {
-        "berlin": (52.3418234221, 13.0882097323, 52.6697240587, 13.7606105539)
+        "berlin": (52.3418234221, 13.0882097323, 52.6697240587, 13.7606105539),
+        "paris": (48.753020, 2.138901, 48.937837, 2.493896),
+        #"london": (51.344500, -0.388934, 51.643400, 0.194758),
+        #"madrid": (40.312817, -3.831991, 40.561061, -3.524374)
     }
 
-    carbon_berlin = CarbonComputation("berlin", bounding_boxes["berlin"])
+    # Iterate over the bounding boxes and schedule a separate thread for each box
+    for city, bounding_box in bounding_boxes.items():
+        username = usernames.get(city)
+        password = passwords.get(city)
 
-    schedule.every(1).minutes.do(
-        run_threaded,
-        update_total_co2_emission_job,
-        args=(username, password, carbon_berlin),
-    )
+        if username and password:
+            carbon_computation = CarbonComputation(city, bounding_box)
+            schedule.every(1).minutes.do(
+                run_threaded,
+                update_total_co2_emission_job,
+                args=(username, password, carbon_computation),
+            )
+        else:
+            print(f"Missing credentials for {city}. Skipping...")
+
     # start the first job now instead of waiting 1 minute
     schedule.run_all()
 
