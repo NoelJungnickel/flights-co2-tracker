ec366bdc0e3434f1efd2d1238022ed7429d16705
kilianp14 <Paul-Kilian@gmx.de>
2023-07-12 16:18:50 +0200

    yet yet another Workflow fix

--- server/src/main.py
+++ server/src/main.py
@@ -91,7 +91,7 @@ def main() -> None:
     for job in schedule.get_jobs("carbon_computation"):
         job.run()
 
-    # Call opensky api every (1 + calculation time) minute(s)
+    # Use schedule
     while True:
         schedule.run_pending()
         time.sleep(1)
