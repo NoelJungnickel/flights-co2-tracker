7465fc4e564d5a5f90042784f52cabf85caed74d
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-07-17 22:32:44 +0200

    Fixed flex ordering issues

--- client/app/components/LeaderboardCard.tsx
+++ client/app/components/LeaderboardCard.tsx
@@ -1,31 +1,26 @@
 import { celebPicturesURL } from "~/utils/celeb_pics_url";
 import CelebAvatar from "./CelebAvatar";
-
-export type LeaderboardEntry = {
-  placing: number;
-  name: string;
-  kgCO2: number;
-};
+import { CelebLeaderboard } from "~/routes/_app.stats.$city";
 
 type Props = {
-  leaderboardContent: LeaderboardEntry[];
+  leaderboardContent: CelebLeaderboard;
 };
 
 function LeaderboardCard({ leaderboardContent }: Props) {
   const ascendingSortedLeaderboard = leaderboardContent.sort(
-    (a, b) => a.placing - b.placing
+    (a, b) => b.emissionsInKg - a.emissionsInKg
   );
 
   return (
     <div className="h-fit w-full rounded-lg bg-zinc-700 py-5">
       <div className="flex w-full flex-col justify-center gap-3 pb-6">
         <h1 className="pb-6 text-center text-4xl font-bold text-sky-50">
-          Top 3 Emitters
+          Top 3 Emitters This Month
         </h1>
         <div className="flex w-full flex-col justify-center gap-10 sm:flex-row sm:gap-6 md:gap-20">
           {[
-            ascendingSortedLeaderboard[1],
             ascendingSortedLeaderboard[0],
+            ascendingSortedLeaderboard[1],
             ascendingSortedLeaderboard[2],
           ].map((celeb, index) => {
             const celebPicture = celebPicturesURL.find(
@@ -36,21 +31,21 @@ function LeaderboardCard({ leaderboardContent }: Props) {
               <div
                 key={celeb.name}
                 className={`${
-                  celeb.placing === 1 ? "order-overwrite-first" : ""
-                } flex flex-col justify-end sm:order-none`}
+                  index === 1 ? "sm:order-overwrite-first" : ""
+                } flex flex-col justify-end`}
               >
                 <CelebAvatar
                   name={celeb.name}
                   imgUrl={celebPicture?.url}
-                  placing={celeb.placing}
+                  placing={index + 1}
                 />
                 <div className="flex flex-col gap-2 pt-8">
                   <h1 className="text-center text-xl font-bold text-blue-50">
                     {celeb.name}
                   </h1>
                   <h2 className="text-center text-xl font-bold text-slate-300">
-                    {celeb.kgCO2}
-                    <span className="text-sm">kg</span>
+                    {Math.floor(celeb.emissionsInKg / 1000)}
+                    <span className="text-sm">t</span>
                   </h2>
                 </div>
               </div>
