4686f1aed45af1732363269857ab264c8a0f74a2
AryehSchoefer <aryeh.shmuel@googlemail.com>
2023-07-20 19:02:43 +0200

    Added FAQ design and questions (no answers yet)

---
+++ client/app/components/Accordion.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+type AccordionItem = {
+  title: string;
+  content: string;
+};
+
+type Props = {
+  items: AccordionItem[];
+};
+
+const Accordion = ({ items }: Props) => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  const handleToggle = (index: number) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
+  return (
+    <div className="w-full">
+      {items.map((item, index) => (
+        <div
+          key={index}
+          className="mb-6 rounded-b-lg rounded-t-lg bg-zinc-700 text-slate-200 hover:bg-zinc-600/75"
+        >
+          <div
+            className={"flex cursor-pointer items-center justify-between p-4"}
+            onClick={() => handleToggle(index)}
+          >
+            <span>{item.title}</span>
+            <span>
+              {activeIndex === index ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <polyline points="18 15 12 9 6 15"></polyline>
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                >
+                  <polyline points="6 9 12 15 18 9"></polyline>
+                </svg>
+              )}
+            </span>
+          </div>
+          {activeIndex === index && (
+            <div className="rounded-b-lg border-x-2 border-b-2 border-zinc-700 bg-zinc-800 p-4">
+              {item.content}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Accordion;
--- client/app/routes/_app.faq.tsx
+++ client/app/routes/_app.faq.tsx
@@ -1,4 +1,33 @@
+import Accordion from "~/components/Accordion";
+
 function Faq() {
-  return <div>_app.faq</div>;
+  const questions = [
+    {
+      title: "What is the goal of this page?",
+      content: "Content of Section 1",
+    },
+    {
+      title: "Where does the data come from?",
+      content: "Content of Section 2",
+    },
+    {
+      title: "How accurate is the data?",
+      content: "Content of Section 2",
+    },
+    {
+      title: "How often does the data update?",
+      content: "Content of Section 2",
+    },
+    {
+      title: "How does the data compare?",
+      content: "Content of Section 2",
+    },
+  ];
+
+  return (
+    <div className="w-full lg:w-2/3">
+      <Accordion items={questions} />
+    </div>
+  );
 }
 export default Faq;
